403fedcd021005c4bda897a4d875fe477b90b1e5
Jeff Geerling <geerlingguy@mac.com>
2016-03-13 15:38:13 -0500

    Use Ansible 2.0 when building the box.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,4 +4,7 @@
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 # Install Ansible.
-yum -y install ansible python-setuptools
+yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel
+yum clean all
+easy_install pip
+pip install ansible
