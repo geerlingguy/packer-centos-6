7de3947e4496bb11668c960cde20a2e68be66d8e
Jeff Geerling <geerlingguy@mac.com>
2016-05-26 19:59:29 -0500

    Fixes #9: Upgrade to 6.8.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Packer Example - CentOS 6 minimal Vagrant Box using Ansible provisioner
 
-**Current CentOS Version Used**: 6.7
+**Current CentOS Version Used**: 6.8
 
 This example build configuration installs and configures CentOS 6 x86_64 minimal using Ansible, and then generates two Vagrant box files, for:
 
--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,7 +4,5 @@
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 # Install Ansible.
-yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel
+yum -y install git python-setuptools gcc sudo libffi-devel python-devel openssl-devel ansible
 yum clean all
-easy_install pip
-pip install ansible
